f73661807a33aba6ec1cf589239b1aa86d0b61d4
Emmanuel EFUFA <emmanuel.efufa.ngalle.1997@gmail.com>
2022-05-09 15:23:34 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title:String = 'SYSGEREQ';
+
+  constructor(private dialog:MatDialog){
+
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogComponent);
+     width:"30%"
+  }
 }
